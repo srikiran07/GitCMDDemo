44b8246d7fa68a88105cf01cd41eb7f992beb276
srikiran07 <91299724+srikiran07@users.noreply.github.com>
2021-09-29 07:31:13 +0530

    updated by devloper

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gitcmdapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
